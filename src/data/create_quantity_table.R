2a44642d4d376fe3028b8ff3d01e9b1dc1e20b6b
Thyra <Thyra@users.noreply.github.com>
2019-03-08 17:32:50 -0600

    Finished first implementation of quantity table

--- src/data/create_quantity_table.R
+++ src/data/create_quantity_table.R
@@ -1,4 +1,4 @@
-# Creates the file `data/mocks/results/quantity_table.csv` by evaluating GAF file quantities and information from `data/mocks/raw/n_genes_per_genome.csv`
+# Creates the file `data/results/quantity_table.csv` by evaluating GAF file quantities and information from `data/raw/n_genes_per_genome.csv`
 library(tools)
 
 # Helper function for the functions below: Checks if the argument passed is a data frame (which would indicate
@@ -13,13 +13,13 @@ possibly_read_gaf <- function(gaf_file) {
   stop("Passed argument is neither data frame nor path to GAF file!")
 }
 
-# Pass the path to a GAF file and it will return a table of the aspect occurences
+# Pass the path/df of a GAF file and it will return a table of the aspect occurences
 count_aspects <- function(gaf_file) {
   gaf_file = possibly_read_gaf(gaf_file)
   return(table(gaf_file$aspect))
 }
 
-# Pass the path to a GAF file and it will return how many genes have at least one annotation in each aspect (C,F,P) or any (A)
+# Pass the path/df of a GAF file and it will return how many genes have at least one annotation in each aspect (C,F,P) or any (A)
 count_genes_annotated <- function(gaf_file) {
   gaf_file = possibly_read_gaf(gaf_file)
   return(c(
@@ -29,6 +29,20 @@ count_genes_annotated <- function(gaf_file) {
     "A" = length(unique(gaf_file$db_object_id))
     ))
 }
+
+# Pass the path/df of a GAF file and get back the median number of annotations for each gene in the GAF
+# in the C,F,P aspect and total median across all aspects (A)
+count_median_annotations <- function(gaf_file) {
+  gaf_file = possibly_read_gaf(gaf_file)
+  t = table(gaf_file$db_object_id, gaf_file$aspect)
+  return(c(
+    "C" = median(t[,"C"]),
+    "F" = median(t[,"F"]),
+    "P" = median(t[,"P"]),
+    "A" = median(t[,"C"]+t[,"F"]+t[,"P"])
+  ))
+}
+
 
 # @TODO This way of creating the dataframe and building it row by row is supposed to be very inefficient -- make it more efficient?
 csv = data.frame(
@@ -42,11 +56,15 @@ csv = data.frame(
   genes_annotated_c = 0,
   genes_annotated_f = 0,
   genes_annotated_p = 0,
-  genes_annotated_a = 0
+  genes_annotated_a = 0,
+  median_annotations_per_gene_c = 0,
+  median_annotations_per_gene_f = 0,
+  median_annotations_per_gene_p = 0,
+  median_annotations_per_gene_a = 0
 )
 
-n_genes_per_genome = read.csv("data/mocks/raw/n_genes_per_genome.csv", header = T)
-genomes = list.dirs("data/mocks/raw/go_annotation_sets", recursive = F)
+n_genes_per_genome = read.csv("data/raw/n_genes_per_genome.csv", header = T)
+genomes = list.dirs("data/raw/go_annotation_sets", recursive = F)
 for(genome_path in genomes) {
   genome_name = basename(genome_path)
   print(genome_name)
@@ -55,6 +73,7 @@ for(genome_path in genomes) {
     gaf_df = possibly_read_gaf(gaf_file)
     aspect_counts = count_aspects(gaf_df)
     genes_annotated = count_genes_annotated(gaf_df)
+    medians = count_median_annotations(gaf_df)
     # Add row to csv object:
     csv <- rbind(csv, data.frame(
       genome = genome_name,
@@ -67,12 +86,14 @@ for(genome_path in genomes) {
       genes_annotated_c = genes_annotated[["C"]],
       genes_annotated_f = genes_annotated[["F"]],
       genes_annotated_p = genes_annotated[["P"]],
-      genes_annotated_a = genes_annotated[["A"]]
+      genes_annotated_a = genes_annotated[["A"]],
+      median_annotations_per_gene_c = medians[["C"]],
+      median_annotations_per_gene_f = medians[["F"]],
+      median_annotations_per_gene_p = medians[["P"]],
+      median_annotations_per_gene_a = medians[["A"]]
     ))
   }
 }
 csv <- csv[2:nrow(csv),] # Drop empty dummy row
-print(csv)
 
-write.csv(csv, 'data/mocks/results/quantity_table.csv', row.names = F)
-
+write.csv(csv, 'data/results/quantity_table.csv', row.names = F)
