c1cf885994d3650e3411477e2e9d881565397a0d
Thyra <Thyra@users.noreply.github.com>
2019-10-20 14:20:15 +0200

    Remove annotations with modifiers during cleanup

--- analyses/cleanup/cleanup.cr
+++ analyses/cleanup/cleanup.cr
@@ -16,7 +16,7 @@ end
 # - if obsolete or duplicates is nil (not 0), the entry is removed
 # - otherwise, if the entry already exists, it is updated
 # - if it does not, it is created
-def update_csv(genome, dataset, obsolete, duplicates)
+def update_csv(genome, dataset, obsolete, duplicates, modified)
   table = CSV.parse(File.open("analyses/cleanup/results/cleanup_table.csv")).to_a
   table.shift # Remove header
   if obsolete.nil? || duplicates.nil?
@@ -29,6 +29,7 @@ def update_csv(genome, dataset, obsolete, duplicates)
     when 1
       selected_elements.first[2] = obsolete.to_s
       selected_elements.first[3] = duplicates.to_s
+      selected_elements.first[4] = modified.to_s
     else
       abort("Duplicate entry for #{genome}/#{dataset}")
     end
@@ -36,7 +37,7 @@ def update_csv(genome, dataset, obsolete, duplicates)
   table = table.sort {|a,b| a[0]+a[1] <=> b[0]+b[1]} # Sort by Genome->Dataset
   File.open("analyses/cleanup/results/cleanup_table.csv", "w") do |f|
     CSV.build(f) do |writer|
-      writer.row(["genome","dataset","obsolete","duplicates"])
+      writer.row(["genome","dataset","obsolete","duplicates","modified"])
       table.each {|row| writer.row(row) }
     end
   end
@@ -45,7 +46,7 @@ end
 if ARGV.first == "--delete"
   genome, dataset = filepath2genome_dataset(ARGV[1])
   File.delete("analyses/cleanup/results/#{genome}/#{dataset}.gaf.gz")
-  update_csv(genome, dataset, nil, nil)
+  update_csv(genome, dataset, nil, nil, nil)
 else
   abort "File #{ARGV.first} not found" unless File.exists? ARGV.first
   genome, dataset = filepath2genome_dataset(ARGV.first)
@@ -58,6 +59,7 @@ else
   puts " Processing " + ARGV.first
   n_obsolete = 0
   n_duplicates = 0
+  n_modifiers = 0
   Gzip::Reader.open(ARGV.first) do |infile|
     reader = CSV.new(infile, separator:'\t')
     Dir.mkdir_p("analyses/cleanup/results/#{genome}")
@@ -74,6 +76,10 @@ else
 
         while reader.next
           row = reader.row.to_a
+          unless row[3] == "" || row[3] == "0" # If modifier not empty or 0: skip this line
+            n_modifiers += 1
+            next
+          end
           row[4] = go.main_id_of(row[4])
           if go.is_obsolete?(row[4])
             n_obsolete += 1
@@ -88,5 +94,5 @@ else
       end
     end
   end
-  update_csv(genome, dataset, n_obsolete, n_duplicates)
+  update_csv(genome, dataset, n_obsolete, n_duplicates, n_modifiers)
 end
