e1abe6fef9590a7213648e906228e80f294af081
Dennis Psaroudakis <psaroudakis@ipk-gatersleben.de>
2020-12-16 14:46:53 +0100

    add jackknifing

--- analyses/treebuilding/treebuilding.rb
+++ analyses/treebuilding/treebuilding.rb
@@ -11,7 +11,7 @@ def jaccard_distance(s1, s2)
   1.0 - (s1 & s2).size.to_f / (s1 | s2).size.to_f
 end
 
-def write_distance_matrix(annotation_sets, filepath)
+def write_distance_matrix(annotation_sets, filepath, filemode="w")
   distances = {}
   species = annotation_sets.keys.sort
 
@@ -24,7 +24,7 @@ def write_distance_matrix(annotation_sets, filepath)
     end
   end
 
-  File.open(filepath, "w") do |f|
+  File.open(filepath, filemode) do |f|
     # Print distance matrix
     f.write("   #{annotation_sets.size}\n")
     species.each do | n1 |
@@ -43,9 +43,9 @@ def write_distance_matrix(annotation_sets, filepath)
   end
 end
 
-def write_binary_matrix(annotation_sets, filepath)
+def write_binary_matrix(annotation_sets, filepath, filemode="w")
   columns = annotation_sets.values.map(&:to_a).flatten.uniq
-  File.open(filepath, "w") do |f|
+  File.open(filepath, filemode) do |f|
     f.write("     #{annotation_sets.length}   #{columns.length}\n")
     annotation_sets.each do |species, set|
       f.write species[0..9].ljust(12)
@@ -54,25 +54,66 @@ def write_binary_matrix(annotation_sets, filepath)
   end
 end
 
+def sample_from_set(set, percentage)
+  terms_to_sample = (set.length * (1.0 - percentage.to_f/100)).floor
+  set.to_a.sample(terms_to_sample).to_set
+end
+
+def add_ancestors_to_set(set, ontology)
+  i = 0
+  set.inject(Set.new) do |s, term| 
+    if s.add? term # returns nil if term already present, then also all ancestors are already present
+      s.merge(ontology.ancestors_of(term, s))
+    else
+      s
+    end
+  end
+end
+
 ontology = Ontology.from_json_file("analyses/cleanup/results/GO.json")
 
 Dir.glob("data/desired_trees/*.yaml").each do |desired_tree|
   name = desired_tree.split(".").first.split("/").last
-  FileUtils.mkdir_p("analyses/treebuilding/results/trees/#{name}") unless File.directory? "analyses/treebuilding/results/trees/#{name}"
+  puts name
+
+  # Delete directory if it exists to make sure we don't get overlapping jackknifing stuff
+  FileUtils.rm_rf("analyses/treebuilding/results/trees/#{name}") if File.directory? "analyses/treebuilding/results/trees/#{name}"
+  FileUtils.mkdir_p("analyses/treebuilding/results/trees/#{name}")
 
   yaml = YAML.load(File.read(desired_tree))
 
-  annotation_sets = yaml["species"].each_with_object({}) do |s, a|
-    puts s
+  ancestor_sets = yaml["species"].each_with_object({}) do |s, a|
     a[s] = JSON.parse(File.read("analyses/treebuilding/results/sets/with_ancestors/#{s}.json")).map(&:to_sym).to_set
   end
 
   if yaml["nj"] # Build a Neighbor-Joining tree
-    write_distance_matrix(annotation_sets, "analyses/treebuilding/results/trees/#{name}/distance_matrix.phy")
+    puts "Building neighbor joining tree"
+    write_distance_matrix(ancestor_sets, "analyses/treebuilding/results/trees/#{name}/distance_matrix.phy")
   end
 
   if yaml["parsimony"] # Build parsimony tree
-    write_binary_matrix(annotation_sets, "analyses/treebuilding/results/trees/#{name}/binary_matrix.phy")
+    puts "Building parsimony tree"
+    write_binary_matrix(ancestor_sets, "analyses/treebuilding/results/trees/#{name}/binary_matrix.phy")
+  end
+
+  if yaml.keys.include? "jackknives"
+    puts "Jackknifing trees..."
+    original_sets = yaml["species"].each_with_object({}) do |s, a|
+      a[s] = JSON.parse(File.read("analyses/treebuilding/results/sets/original/#{s}.json")).map(&:to_sym).to_set
+    end
+
+    yaml["jackknives"]["percentages"].each do |p|
+
+      yaml["jackknives"]["n_trees"].times do |jackknife_index|
+        puts " #{p}%, tree #{jackknife_index}"
+        jackknifed_sets_original = original_sets.each_with_object({}) { |(name, set), a| a[name] = sample_from_set(set, p)}
+        jackknifed_sets_with_ancestors = jackknifed_sets_original.each_with_object({}) { |(name, set), a | a[name] = add_ancestors_to_set(set, ontology)}
+
+        write_distance_matrix(ancestor_sets, "analyses/treebuilding/results/trees/#{name}/distance_matrix_jackknifed_#{p}.phy", "a") if yaml["nj"]
+        write_binary_matrix(ancestor_sets, "analyses/treebuilding/results/trees/#{name}/binary_matrix_jackknifed_#{p}.phy", "a") if yaml["parsimony"]
+      end
+
+    end
   end
 end
 
